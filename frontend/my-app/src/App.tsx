d80cf7772ad91e3c5535a494ec73fc49f56fc06b
lokesh75way <lokesh@75way.com>
2025-05-20 19:00:37 +0530

    added the protected route

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import LoginForm from './pages/login';
 import AdminDashboard from './pages/AdminDashboard';
 import UserDashboard from './pages/UserDashboard';
 import RegisterForm from './pages/Register';
 import UserPage from './pages/UserPage';
+import AuthLayout from './layouts/auth';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <Routes> 
       <Route path="/login" element={<LoginForm />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path="/user" element={<UserDashboard />} />
-      <Route path="/user/dashboard" element={<UserPage />} />
+      <Route path="/register" element={<RegisterForm switchToLogin={() => navigate('/login')} />} />
+      <Route path="/admin/dashboard" element={
+        <AuthLayout requireAdmin>
+          <AdminDashboard />
+        </AuthLayout>
+      } />
+      <Route path="/user" element={
+        <AuthLayout>
+          <UserDashboard />
+        </AuthLayout>
+      } />
+      <Route path="/user/dashboard" element={
+        <AuthLayout>
+          <UserPage />
+        </AuthLayout>
+      } />
     </Routes>
   );
 }
-        
 
 export default App;
