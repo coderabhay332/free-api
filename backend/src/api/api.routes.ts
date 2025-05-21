d0ea22df5029c867904ea3ae98d803afff24b558
lokesh75way <lokesh@75way.com>
2025-05-21 11:21:31 +0530

    added the rate limit

--- backend/src/api/api.routes.ts
+++ backend/src/api/api.routes.ts
@@ -3,19 +3,19 @@ import passport from "passport";
 import { catchError } from "../common/middleware/catch-error";
 import { roleAuth } from "../common/middleware/role-auth.middleware";
 import * as apiController from "./api.controller";
-
+import { limiter } from "../common/helper/rate-limiter";
 const router = Router();
 
 
 router
-.post("/create", roleAuth(["ADMIN"]), catchError, apiController.createApi)
+.post("/create", limiter, roleAuth(["ADMIN"]), catchError, apiController.createApi)
 
-.get("/all", roleAuth(["ADMIN", "USER"]), catchError, apiController.getAllApis)
-.post("/demo/:id", roleAuth(["ADMIN", "USER"]), catchError, apiController.demoApi)
+.get("/all", limiter, roleAuth(["ADMIN", "USER"]), catchError, apiController.getAllApis)
+.post("/demo/:id", limiter, roleAuth(["ADMIN", "USER"]), catchError, apiController.demoApi)
 
-.get("/:id", roleAuth(["ADMIN", "USER"]), catchError, apiController.getApiById)
+.get("/:id", limiter, roleAuth(["ADMIN", "USER"]), catchError, apiController.getApiById)
 
 
-.post("/subscribe/:id", roleAuth(["USER", "ADMIN"]), catchError, apiController.subscribeApi);
+.post("/subscribe/:id", limiter, roleAuth(["USER", "ADMIN"]), catchError, apiController.subscribeApi);
 
     export default router;
--- backend/src/user/user.routes.ts
+++ backend/src/user/user.routes.ts
@@ -4,13 +4,14 @@ import { catchError } from "../common/middleware/catch-error";
 import { roleAuth } from "../common/middleware/role-auth.middleware";
 import * as userController from "./user.controllers";
 import * as userValidator from "./user.validation";
+import { limiter } from "../common/helper/rate-limiter";
 
 
 const router = Router();
 router
-            .post("/register", userValidator.createUser, catchError, userController.createUser)
-            .post("/login", userValidator.login, catchError, passport.authenticate('login', { session: false }), userController.login)
-            .post("/subscribe/:id", roleAuth(["USER", "ADMIN"]), catchError, userController.subscribeApi)
-            .get("/me", roleAuth(["USER", "ADMIN"]), catchError, userController.getMe);
+            .post("/register", limiter, userValidator.createUser, catchError, userController.createUser)
+            .post("/login", limiter, userValidator.login, catchError, passport.authenticate('login', { session: false }), userController.login)
+            .post("/subscribe/:id", limiter, roleAuth(["USER", "ADMIN"]), catchError, userController.subscribeApi)
+            .get("/me", limiter, roleAuth(["USER", "ADMIN"]), catchError, userController.getMe);
 
             export default router;
